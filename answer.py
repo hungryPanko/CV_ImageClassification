87c9c122f820aec875477bf0985d37bd5a4b5e9d
Hanna Lee <101350589+hungryPanko@users.noreply.github.com>
2023-04-10 16:07:14 -0700

    Add files via upload

---
+++ answer.py
@@ -0,0 +1,109 @@
+import pdb
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import transforms
+
+
+# %%
+class NN(nn.Module):
+    def __init__(self, arr=[]):
+        super(NN, self).__init__()
+        self.relu = nn.ReLU()
+        self.fc1 = nn.Linear(30 * 30 * 3, 128) # 30x30 image * 3 channels
+        self.fc2 = nn.Linear(128, 5)
+
+    def forward(self, x):           # x shape: [64, 3, 30, 30]
+        batch_size = x.shape[0]     # 64
+        x = x.view(batch_size, -1)  # x shape: [64, 2700]
+        x = self.fc1(x)             # 64, 128
+        x = self.relu(x)            # 64, 128
+        x = self.fc2(x)             # 64, 5
+        return x
+
+
+# %%
+class SimpleCNN(nn.Module):
+    def __init__(self, arr=[]):
+        super(SimpleCNN, self).__init__()
+        self.conv_layer = nn.Conv2d(3, 8, 3) # in_channels, out_channels, kernel_size
+        self.pool = nn.MaxPool2d(2)
+        self.fc1 = nn.Linear(1568, 5)
+
+    def forward(self, x):
+        """
+        Question 2
+        TODO: fill this forward function for data flow
+        """
+        batch_size = x.shape[0]                     # x shape: 64
+        x = self.pool(F.relu(self.conv_layer(x)))    
+        x = x.view(batch_size, 1568)                # x shape: 64, 1568
+        x = self.fc1(x)                             # x shape: 64, 5
+        return x
+
+# %%
+basic_transformer = transforms.Compose([transforms.ToTensor()])
+
+"""
+Question 3
+TODO: Add color normalization to the transformer. For simplicity, let us use 0.5 for mean
+      and 0.5 for standard deviation for each color channel.
+"""
+norm_transformer = transforms.Compose([ 
+    transforms.ToTensor(), 
+    transforms.Normalize( mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+])
+
+# %%
+class DeepCNN(nn.Module):
+    def __init__(self, arr=[]): #arr = [8, 16, 32]
+        super(DeepCNN, self).__init__()
+        """
+        Question 4
+        TODO: setup the structure of the network
+        """
+        self.seq = nn.Sequential(  # nn.Conv2(): stride=1, padding=0, dilation=1, groups=1 is default 
+            nn.Conv2d(3,arr[0],3), # in_channels(3), out_channels(8), kernel_size(3), 
+            nn.ReLU(),
+            nn.Conv2d(arr[0],arr[1],3), # in_channels(8), out_channels(16), kernel_size(3)
+            nn.ReLU(),
+            nn.Conv2d(arr[1],arr[2],3), # in_channels(16), out_channels(32), kernel_size(3)
+            nn.ReLU(),
+            nn.MaxPool2d(2), # stride=None, padding=0, dilation=1 is default
+        )
+        self.fc = nn.Linear(12*12*arr[2], 5)
+
+    def forward(self, x):
+        """
+        Question 4
+        TODO: setup the flow of data (tensor)
+        """
+        batch_size = x.shape[0]  
+        x = self.seq(x)
+        x = x.view(batch_size, 12*12*32)   # x shape: 64, 12*12*32
+        x = self.fc(x)                     # x shape: 64, 5
+        return x
+
+
+# %%
+"""
+Question 5
+TODO:
+    change the train_transformer to a tranformer with random horizontal flip
+    and random affine transformation
+    1. It should randomly flip the image horizontally with probability 50%
+    2. It should apply random affine transformation to the image, which randomly rotate the image 
+        within 5 degrees, and shear the image within 10 degrees.
+    3. It should include color normalization after data augmentation. Similar to question 3.
+"""
+
+"""Add random data augmentation to the transformer"""
+#  Use the torchvision.transforms package to finish the variable aug transformer. 
+aug_transformer = transforms.Compose([
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomAffine(degrees=5, shear=10),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+])
